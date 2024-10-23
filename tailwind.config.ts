acee0502f1361f88e6a0feb4b221d6c299608f95
hax <admin@nullhax.com>
2024-10-23 15:24:08 -0700

    add alma specific styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,10 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "alma-green": "#d9dea5",
+      },
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
       },
     },
   },
